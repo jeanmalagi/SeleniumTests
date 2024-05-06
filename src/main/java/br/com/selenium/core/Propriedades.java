3ec293a2ec04480930133c345514005d46b45934
idd_jmalagi <idd_jmalagi@uolinc.com>
2024-05-06 14:56:14 -0300

    Inclusão de projeto base para automação

---
+++ src/main/java/br/com/selenium/core/Propriedades.java
@@ -0,0 +1,15 @@
+package br.com.selenium.core;
+
+public class Propriedades {
+
+    public static boolean FECHAR_BROWSER = false;
+
+    public static Browsers browser = Browsers.CHROME;
+
+    public enum Browsers {
+        CHROME,
+        FIREFOX
+    }
+
+}
+
