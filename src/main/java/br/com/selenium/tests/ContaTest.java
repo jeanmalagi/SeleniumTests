91549e488f2dccc48e4f146a0c6b20f48c5fa963
Jean Malagi <jeanmalagi29@gmail.com>
2024-05-19 17:47:24 -0300

    Inclui classe ContaTest para execução dos testes

---
+++ src/main/java/br/com/selenium/tests/ContaTest.java
@@ -0,0 +1,12 @@
+package br.com.selenium.tests;
+
+import br.com.selenium.core.BaseTest;
+import org.junit.Test;
+
+public class ContaTest extends BaseTest {
+
+    @Test
+    public void test(){
+
+    }
+}
