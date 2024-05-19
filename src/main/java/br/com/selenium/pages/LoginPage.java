d1b047dee957d03293c002dc31b358bb56f337cf
Jean Malagi <jeanmalagi29@gmail.com>
2024-05-19 17:47:00 -0300

    Inclui loginPage

---
+++ src/main/java/br/com/selenium/pages/LoginPage.java
@@ -0,0 +1,32 @@
+package br.com.selenium.pages;
+
+import br.com.selenium.core.BasePage;
+import br.com.selenium.core.DriverFactory;
+import org.openqa.selenium.By;
+
+public class LoginPage extends BasePage {
+
+    public void acessarTelaInicial(){
+        DriverFactory.getDriver().get("https://seubarriga.wcaquino.me/");
+    }
+
+    public void setEmail(String email) {
+        escrever("email", email);
+    }
+
+    public void setSenha(String senha) {
+        escrever("senha", senha);
+    }
+
+    public void entrar(){
+        clicarBotao(String.valueOf(By.xpath("//button[.='Entrar']")));
+    }
+
+    public void logar(String email, String senha) {
+        setEmail(email);
+        setSenha(senha);
+        entrar();
+    }
+
+}
+
