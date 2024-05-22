cb3b792eab144882e5faca49b7c434317ad672bb
Jean Malagi <jeanmalagi29@gmail.com>
2024-05-22 17:01:54 -0300

    inclui teste para acesso a tela de menu

--- src/main/java/br/com/selenium/tests/ContaTest.java
+++ src/main/java/br/com/selenium/tests/ContaTest.java
@@ -1,12 +1,16 @@
 package br.com.selenium.tests;
 
 import br.com.selenium.core.BaseTest;
+import br.com.selenium.pages.MenuPage;
 import org.junit.Test;
 
 public class ContaTest extends BaseTest {
 
+    MenuPage menuPage = new MenuPage();
     @Test
     public void test(){
+
+        menuPage.acessarTelaInserirConta();
 
     }
 }
