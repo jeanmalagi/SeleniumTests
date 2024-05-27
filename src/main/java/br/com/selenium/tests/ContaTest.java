2b428d251b7854b43d26a94cb5eac190cd29b332
Jean Malagi <jeanmalagi29@gmail.com>
2024-05-27 12:32:57 -0300

    inclui teste para inclusão de conta

--- src/main/java/br/com/selenium/tests/ContaTest.java
+++ src/main/java/br/com/selenium/tests/ContaTest.java
@@ -1,16 +1,23 @@
 package br.com.selenium.tests;
 
 import br.com.selenium.core.BaseTest;
+import br.com.selenium.pages.ContaPage;
 import br.com.selenium.pages.MenuPage;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ContaTest extends BaseTest {
 
     MenuPage menuPage = new MenuPage();
+    ContaPage contaPage = new ContaPage();
     @Test
     public void test(){
 
         menuPage.acessarTelaInserirConta();
 
+        contaPage.setNome("Conta 1");
+        contaPage.salvar();
+
+        Assert.assertEquals("Conta adicionada com sucesso!", contaPage.obterMensagemSucesso());
     }
 }
