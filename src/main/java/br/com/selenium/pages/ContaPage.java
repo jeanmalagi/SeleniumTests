e96f43c5d3c27ce5c310a08073a9c9c421e913eb
Jean Malagi <jeanmalagi29@gmail.com>
2024-06-14 15:28:03 -0300

    inclui acessos as pages para alterar conta

--- src/main/java/br/com/selenium/pages/ContaPage.java
+++ src/main/java/br/com/selenium/pages/ContaPage.java
@@ -16,4 +16,13 @@ public class ContaPage extends BasePage {
     public String obterMensagemSucesso(){
         return obterTexto(By.xpath("//div[@class='alert alert-success']"));
     }
+
+    public String obterMensagemErro(){
+        return obterTexto(By.xpath("//div[@class='alert alert-danger']"));
+    }
+
+    public void clicarAlterarConta(String string) {
+        obterCelula("Conta", string, "Ações", "tabelaContas")
+                .findElement(By.xpath(".//span[@class='glyphicon glyphicon-edit']")).click();
+    }
 }
